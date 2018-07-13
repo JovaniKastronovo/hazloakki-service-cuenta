e9e19e4b438cda6332fd5d887481a0f42a73359a
jarzatec <jovaniac@gmail.com>
2018-07-13 12:36:31 -0500

    microservicio con postgres funciona ok

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,12 +1,11 @@
-CREATE TABLE `cuenta` (
-  `id` varchar(255) NOT NULL,
-  `apellidos` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `estatus` bit(1) NOT NULL,
-  `fecha` varchar(255) DEFAULT NULL,
-  `nombre` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `telefono` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE cuenta (
+  id VARCHAR(255) PRIMARY KEY,
+  apellidos varchar(255) DEFAULT NULL,
+  email varchar(255) DEFAULT NULL,
+  estatus  BOOLEAN NOT NULL DEFAULT FALSE,
+  fecha varchar(255) DEFAULT NULL,
+  nombre varchar(255) DEFAULT NULL,
+  password varchar(255) DEFAULT NULL,
+  telefono varchar(255) DEFAULT NULL
+);
 
